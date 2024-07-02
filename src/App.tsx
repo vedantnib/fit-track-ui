c88ae89682878f50a0d8508fca07e56735b1c341
Vedant Nibandhe <vnibandhe@gmail.com>
2024-07-01 23:53:01 -0500

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Vite and React with Tailwind CSS!</h1>
+    </div>
+    </div>
+  )
+}
+
+export default App
